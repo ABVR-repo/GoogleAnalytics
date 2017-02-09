d91c86b8691506325d5b49887da9001dad218d93
gameDNAstudio <office@gamednastudio.com>
2017-02-09 19:17:23 +0100

    Update credits

--- Source/GoogleAnalytics/Private/GoogleAnalyticsSettings.cpp
+++ Source/GoogleAnalytics/Private/GoogleAnalyticsSettings.cpp
@@ -1,6 +1,6 @@
 // Google Analytics Provider
 // Created by Patryk Stepniewski
-// Copyright (c) 2014-2017 gameDNA studio. All Rights Reserved.
+// Copyright (c) 2014-2017 gameDNA. All Rights Reserved.
 
 #include "GoogleAnalyticsPrivatePCH.h"
 #include "GoogleAnalyticsSettings.h"
