d6abe819d744b798f1257fcb805a89ac27bf0cd9
gameDNAstudio <office@gamednastudio.com>
2017-06-26 23:46:49 +0200

    Update copyrights

--- Source/GoogleAnalytics/GoogleAnalytics.Build.cs
+++ Source/GoogleAnalytics/GoogleAnalytics.Build.cs
@@ -1,6 +1,6 @@
 // Google Analytics Provider
 // Created by Patryk Stepniewski
-// Copyright (c) 2014-2017 gameDNA. All Rights Reserved.
+// Copyright (c) 2014-2017 gameDNA Ltd. All Rights Reserved.
 
 using System.IO;
 
